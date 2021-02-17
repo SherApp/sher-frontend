5cdfe8636091c3129e67898ba69126a4c88ba206
Piotr Romanowski <wsrh456@gmail.com>
2021-02-17 21:17:40 +0100

    Add copy link to clipboard functionality

--- src/features/upload/UploadLink.tsx
+++ src/features/upload/UploadLink.tsx
@@ -1,19 +1,28 @@
-import Typography from '../../components/Typography';
 import clipboardIcon from '../../img/clipboard.svg';
+import { useRef } from 'react';
 
 interface UploadLinkProps {
   link?: string;
 }
 
 const UploadLink = ({ link }: UploadLinkProps) => {
+  const linkRef = useRef<HTMLInputElement>(null);
+
+  const handleCopyClick = () => {
+    linkRef.current?.select();
+    document.execCommand('copy');
+  };
+
   return (
     <div className="flex rounded-lg overflow-hidden">
-      <div className="flex-grow w-0 bg-gray-200 whitespace-nowrap flex items-center scrollbar-thumb-rounded scrollbar-thin scrollbar-thumb-purple scrollbar-track-gray-300">
-        <Typography className="px-2 py-1" component="p">
-          {link}
-        </Typography>
-      </div>
+      <input
+        ref={linkRef}
+        className="flex flex-grow bg-gray-200 px-2 py-1 w-full bg-transparent outline-none"
+        readOnly
+        value={link}
+      />
       <button
+        onClick={handleCopyClick}
         className="bg-gradient-r-purple-pink p-1"
         aria-label="copy to clipboard"
       >
