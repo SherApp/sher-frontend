49f86e0c8251b4cc3a57386a443adad16a890c1f
Piotr Romanowski <wsrh456@gmail.com>
2021-03-25 18:24:39 +0100

    Fix AccountMenu tests

--- src/features/auth/AccountMenu.test.tsx
+++ src/features/auth/AccountMenu.test.tsx
@@ -1,16 +1,26 @@
 import AccountMenu from './AccountMenu';
 import { fireEvent, render } from '@testing-library/react';
 import { signOut } from './apiCalls';
+import { useHistory } from 'react-router-dom';
 
 jest.mock('./apiCalls', () => ({
   signOut: jest.fn()
 }));
 
-it('signs out on sign out click', () => {
+jest.mock('react-router-dom', () => ({
+  useHistory: jest.fn()
+}));
+
+it('signs out on sign out click', async () => {
+  (useHistory as jest.Mock).mockReturnValue({
+    push: jest.fn()
+  });
+
   const { getByLabelText, getByText } = render(<AccountMenu />);
 
   fireEvent.click(getByLabelText(/account menu/i));
-  fireEvent.click(getByText(/sign out/i));
+  await fireEvent.click(getByText(/sign out/i));
 
   expect(signOut).toHaveBeenCalled();
+  expect(useHistory().push).toHaveBeenCalledWith('/signIn');
 });
