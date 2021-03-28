14c9799636475cdc98830e090aaf26d10d1f9d98
Piotr Romanowski <wsrh456@gmail.com>
2021-03-28 16:02:28 +0200

    Test showing admin area menu item

--- src/features/auth/AccountMenu.test.tsx
+++ src/features/auth/AccountMenu.test.tsx
@@ -1,26 +1,43 @@
 import AccountMenu from './AccountMenu';
-import { fireEvent, render } from '@testing-library/react';
-import { signOut } from './apiCalls';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { getUser, signOut } from './apiCalls';
 import { useHistory } from 'react-router-dom';
 
 jest.mock('./apiCalls', () => ({
-  signOut: jest.fn()
+  signOut: jest.fn(),
+  getUser: jest.fn()
 }));
 
 jest.mock('react-router-dom', () => ({
   useHistory: jest.fn()
 }));
 
-it('signs out on sign out click', async () => {
+beforeEach(() => {
   (useHistory as jest.Mock).mockReturnValue({
     push: jest.fn()
   });
+});
+
+it('signs out on sign out click', async () => {
+  (getUser as jest.Mock).mockResolvedValue({ roles: [] });
 
   const { getByLabelText, getByText } = render(<AccountMenu />);
 
-  fireEvent.click(getByLabelText(/account menu/i));
+  await waitFor(() => {
+    fireEvent.click(getByLabelText(/account menu/i));
+  });
   await fireEvent.click(getByText(/sign out/i));
 
   expect(signOut).toHaveBeenCalled();
   expect(useHistory().push).toHaveBeenCalledWith('/signIn');
+});
+
+it('shows admin route for user with "Admin" role', async () => {
+  (getUser as jest.Mock).mockResolvedValue({ roles: ['Admin'] });
+
+  const { getByText } = render(<AccountMenu />);
+
+  await waitFor(() => {
+    expect(getByText(/admin area/i)).toBeInTheDocument();
+  });
 });
