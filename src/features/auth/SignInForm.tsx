68df0357baad73168cbe62a9b5948ad74ff5976f
Piotr Romanowski <wsrh456@gmail.com>
2021-04-02 15:10:32 +0200

    Use Formik

--- src/features/auth/SignInForm.tsx
+++ src/features/auth/SignInForm.tsx
@@ -7,34 +7,28 @@ import { useState } from 'react';
 import EllipsisLoading from '../../components/EllipsisLoading';
 import { signIn } from './apiCalls';
 import { useHistory } from 'react-router-dom';
+import { Field, Form, Formik } from 'formik';
 
 interface Props {
   returnPath: string;
+}
+
+interface Values {
+  emailAddress: string;
+  password: string;
 }
 
 const SignInForm = ({ returnPath }: Props) => {
   const history = useHistory();
 
   const [signingIn, setSigningIn] = useState(false);
-  const [emailAddress, setEmailAddress] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const handleSubmit = async ({ emailAddress, password }: Values) => {
     setSigningIn(true);
 
     await signIn({ emailAddress, password });
 
     history.push(returnPath);
-  };
-
-  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailAddress(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
   };
 
   if (signingIn) {
@@ -46,30 +40,43 @@ const SignInForm = ({ returnPath }: Props) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex px-3 space-y-12 flex-col">
-      <TextInput
-        endAdornment={
-          <InputAdornment>
-            <img src={userIcon} alt="" />
-          </InputAdornment>
-        }
-        label="Email"
-        type="email"
-        placeholder="example@example.com"
-        onChange={handleUsernameChange}
-      />
-      <TextInput
-        endAdornment={
-          <InputAdornment>
-            <img src={passwordIcon} alt="" />
-          </InputAdornment>
-        }
-        label="Password"
-        type="password"
-        onChange={handlePasswordChange}
-      />
-      <Button type="submit">Sign in</Button>
-    </form>
+    <Formik
+      initialValues={{ emailAddress: '', password: '' }}
+      onSubmit={handleSubmit}
+      className="flex px-3 space-y-12 flex-col"
+    >
+      <Form className="px-3">
+        <div className="space-y-12 mb-12">
+          <Field
+            as={TextInput}
+            name="emailAddress"
+            label="Email"
+            type="email"
+            placeholder="example@example.com"
+            endAdornment={
+              <InputAdornment>
+                <img src={userIcon} alt="" />
+              </InputAdornment>
+            }
+          />
+          <Field
+            as={TextInput}
+            name="password"
+            label="Password"
+            type="password"
+            endAdornment={
+              <InputAdornment>
+                <img src={passwordIcon} alt="" />
+              </InputAdornment>
+            }
+          />
+        </div>
+        <div className="flex flex-col space-y-4">
+          <Button type="submit">Sign in</Button>
+          <Button variant="secondary">Sign up</Button>
+        </div>
+      </Form>
+    </Formik>
   );
 };
 
