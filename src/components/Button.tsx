d73b24361915c411b9f19dd12e869267246df03f
Piotr Romanowski <wsrh456@gmail.com>
2021-04-02 14:21:12 +0200

    Create secondary button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import clsx from 'clsx';
 import Typography from './Typography';
 
+type Variant = 'primary' | 'secondary';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: Variant;
+}
+
 const Button = ({
   className,
   children,
+  variant = 'primary',
   ...rest
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+}: ButtonProps) => {
   const classes = clsx(
-    'bg-gradient-r-purple-pink rounded py-2 text-white shadow-button px-10',
+    [
+      variant === 'primary' &&
+        'bg-gradient-r-purple-pink shadow-button text-white'
+    ],
+    [variant === 'secondary' && 'text-pink border-pink border'],
+    'rounded py-2 px-10',
     className
   );
 
