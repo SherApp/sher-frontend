c8526a44568b47c9c9542d25594dbf5a604c7bff
Piotr Romanowski <wsrh456@gmail.com>
2021-04-03 14:52:08 +0200

    AuthRoute layout improvements

--- src/features/auth/AuthRoute.tsx
+++ src/features/auth/AuthRoute.tsx
@@ -8,11 +8,11 @@ const AuthRoute = () => {
   const { pathname } = useLocation();
   const isSignIn = pathname === routes.auth('signIn');
   return (
-    <div className="w-full justify-center items-center flex flex-col flex-grow">
+    <div className="w-full justify-center items-center flex flex-col flex-grow my-10">
       <Handwriting variant="h2" component="h1" className="py-4 mb-6">
         sher.lol
       </Handwriting>
-      <div className="md:w-1/2 xl:w-1/4 relative">
+      <div className="md:w-1/2 2xl:w-1/4 relative">
         <div className="w-full h-full bg-gradient-r-purple-pink absolute transform rotate-6 rounded-xl top-0 left-0 -z-10" />
         <div className="px-8 py-10 shadow-xl bg-white dark:bg-gray-800 rounded-xl">
           <Handwriting variant="h3" component="h2" className="py-4 mb-6">
