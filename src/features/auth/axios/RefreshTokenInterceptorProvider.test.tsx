9688bcf373375a06639a86fd7c1b3afe9e4cd054
Piotr Romanowski <wsrh456@gmail.com>
2021-04-05 13:03:02 +0200

    Fix tests

--- src/features/auth/axios/RefreshTokenInterceptorProvider.test.tsx
+++ src/features/auth/axios/RefreshTokenInterceptorProvider.test.tsx
@@ -2,6 +2,12 @@ import { render } from '@testing-library/react';
 import RefreshTokenInterceptorProvider from './RefreshTokenInterceptorProvider';
 import { refreshTokenInterceptor } from './refreshTokenInterceptor';
 import apiClient from '../../../api/apiClient';
+import { useHistory, useLocation } from 'react-router-dom';
+
+jest.mock('react-router-dom', () => ({
+  useHistory: jest.fn(),
+  useLocation: jest.fn()
+}));
 
 jest.mock('./refreshTokenInterceptor');
 
@@ -10,6 +16,12 @@ it('uses refresh token interceptor', () => {
   (refreshTokenInterceptor as jest.Mock).mockImplementationOnce(
     () => interceptor
   );
+  (useLocation as jest.Mock).mockReturnValue({
+    pathname: '/'
+  });
+  (useHistory as jest.Mock).mockReturnValue({
+    push: jest.fn()
+  });
 
   const spy = jest.spyOn(apiClient.interceptors.response, 'use');
 
