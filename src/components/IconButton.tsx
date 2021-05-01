fc5de9eec09a4343c21d01ef6595066d46c5fe56
Piotr Romanowski <wsrh456@gmail.com>
2021-05-02 01:24:35 +0200

    Fix IconButton styles

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -7,7 +7,9 @@ interface Props extends React.HTMLAttributes<HTMLButtonElement> {
 const IconButton = ({ gradient, className, ...rest }: Props) => {
   const classes = clsx(
     'p-1 rounded-lg transition-colors',
-    gradient ? 'bg-gradient-r-purple-pink' : 'bg-transparent hover:bg-gray-700',
+    gradient
+      ? 'bg-gradient-r-purple-pink'
+      : 'bg-transparent hover:bg-gray-200 dark:hover:bg-gray-700',
     className
   );
   return <button className={classes} {...rest} />;
