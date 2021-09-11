bce285fc3cce45c0334b8e58bea64acb222b2926
Piotr Romanowski <wsrh456@gmail.com>
2021-09-11 13:21:45 +0200

    Create FileManager component

--- src/features/browse/BrowseRoute.tsx
+++ src/features/browse/BrowseRoute.tsx
@@ -1,64 +1,21 @@
-import useDirectory from './useDirectory';
-import React, { useState } from 'react';
-import Button from '../../components/Button';
-import { FolderPlus } from 'react-feather';
-import CreateFolderDialog from './CreateFolderDialog';
+import React from 'react';
 import {
   FileDragAreaInfo,
   FileDragAreaContextProvider
 } from '../../components/FileDragArea';
 import PendingUploads from '../upload/PendingUploads';
-import { useRouter } from 'next/router';
-import PathBreadcrumbs from './PathBreadcrumbs';
-import DirectoryContentsTable from './DirectoryContentsTable';
 import Bookmarks from './Bookmarks';
 import ItemInfo from './ItemInfo';
+import FileManager from './FileManager';
 
 const BrowseRoute = () => {
-  const [showCreateFolderDialog, setShowCreateFolderDialog] = useState(false);
-
-  const { query } = useRouter();
-
-  const { directory, createChildDirectory } = useDirectory(
-    (query?.id as string) ?? undefined
-  );
-
-  const handleCreateFolderClick = () => {
-    setShowCreateFolderDialog(true);
-  };
-
-  const handleCreateFolder = async (name?: string) => {
-    if (!name) return;
-
-    setShowCreateFolderDialog(false);
-    await createChildDirectory(name);
-  };
-
-  if (!directory) return null;
-
-  const files = directory?.files;
-  const directories = directory?.directories;
-
   return (
     <FileDragAreaContextProvider>
       <PendingUploads />
       <FileDragAreaInfo />
       <div className="px-8 flex">
         <Bookmarks className="w-60" />
-        <div className="flex-grow">
-          <CreateFolderDialog
-            onOkClick={handleCreateFolder}
-            onClose={() => setShowCreateFolderDialog(false)}
-            show={showCreateFolderDialog}
-          />
-          <PathBreadcrumbs path={directory?.path} />
-          <div className="mb-2">
-            <Button onClick={handleCreateFolderClick} icon={<FolderPlus />}>
-              Create folder
-            </Button>
-          </div>
-          <DirectoryContentsTable data={[...files, ...directories]} />
-        </div>
+        <FileManager className="flex-grow" />
         <ItemInfo className="w-60" name="ok" />
       </div>
     </FileDragAreaContextProvider>
---
+++ src/features/browse/FileManager/FileManager.tsx
@@ -0,0 +1,57 @@
+import CreateFolderDialog from '../CreateFolderDialog';
+import PathBreadcrumbs from '../PathBreadcrumbs';
+import Button from '../../../components/Button';
+import { FolderPlus } from 'react-feather';
+import DirectoryContentsTable from './DirectoryContentsTable';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import useDirectory from '../useDirectory';
+
+interface Props {
+  className?: string;
+}
+
+const FileManager = ({ className }: Props) => {
+  const [showCreateFolderDialog, setShowCreateFolderDialog] = useState(false);
+
+  const { query } = useRouter();
+
+  const { directory, createChildDirectory } = useDirectory(
+    (query?.id as string) ?? undefined
+  );
+
+  const handleCreateFolderClick = () => {
+    setShowCreateFolderDialog(true);
+  };
+
+  const handleCreateFolder = async (name?: string) => {
+    if (!name) return;
+
+    setShowCreateFolderDialog(false);
+    await createChildDirectory(name);
+  };
+
+  if (!directory) return null;
+
+  const files = directory?.files;
+  const directories = directory?.directories;
+
+  return (
+    <div className={className}>
+      <CreateFolderDialog
+        onOkClick={handleCreateFolder}
+        onClose={() => setShowCreateFolderDialog(false)}
+        show={showCreateFolderDialog}
+      />
+      <PathBreadcrumbs path={directory?.path} />
+      <div className="mb-2">
+        <Button onClick={handleCreateFolderClick} icon={<FolderPlus />}>
+          Create folder
+        </Button>
+      </div>
+      <DirectoryContentsTable data={[...files, ...directories]} />
+    </div>
+  );
+};
+
+export default FileManager;
